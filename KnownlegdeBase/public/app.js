d65e63366156c930cdfcf8f2b1c617b7c245b5ee
Ngo Hung Phuc <ngohungphuc95@gmail.com>
2016-06-03 23:46:26 +0700

    Angular Routes & Views

---
+++ KnownlegdeBase/public/app.js
@@ -0,0 +1,21 @@
+var app = angular.module('kB',['ngRoute']);
+
+app.config(['$routeProvider',function($routeProvider){
+	$routeProvider.when('/articles', {
+		templateUrl: 'views/articles.view.html',
+		controller: 'ArticlesCtrl'
+	}).when('/articles/details/:id', {
+		templateUrl: 'views/article_detail.view.html',
+		controller: 'ArticlesDetailCtrl'
+	}).when('/articles/category/:category', {
+		templateUrl: 'views/cat_article.view.html',
+		controller: 'ArticlesCategoryCtrl'
+	}).when('/articles/add', {
+		templateUrl: 'views/add_article.view.html',
+		controller: 'ArticlesAddCtrl'
+	}).when('/articles/edit/:id', {
+		templateUrl: 'views/edit_article.view.html',
+		controller: 'ArticlesEditCtrl'
+	}).
+	otherwise({redirectTo: '/articles'})
+}]);
