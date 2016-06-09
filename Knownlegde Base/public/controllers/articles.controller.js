071ca01c0b225e8243108d122e56e93a0760f1ab
Ngo Hung Phuc <ngohungphuc95@gmail.com>
2016-06-09 19:59:26 +0700

    Sails Server Setup

--- Knownlegde Base/public/controllers/articles.controller.js
+++ Knownlegde Base/public/controllers/articles.controller.js
@@ -8,7 +8,6 @@ angular.module('kB')
 .controller('ArticlesCtrl', ['$scope','$http', function ($scope,$http) {
 	$http.get('/articles').success(function(data){
 		$scope.articles=data;
-
 	});
 }])
 
