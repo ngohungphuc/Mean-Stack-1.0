6cd2055a41ea26a5833283cf836380c5b8d4322b
Ngo Hung Phuc <ngohungphuc95@gmail.com>
2016-06-10 00:25:46 +0700

    Backend Signup

--- User Authentication/api/controllers/UserController.js
+++ User Authentication/api/controllers/UserController.js
@@ -5,7 +5,46 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
-module.exports = {
-	
+ module.exports = {
+	//singup function
+	singup : function(req,res){
+		//require module
+		var Passwords = require('machinepack-passwords');
+		Passwords.encryptPassword({
+			password: req.param('password'),
+			difficulty:6,
+		}).exec({
+			error: function(err){
+				return res.negotiate(err);
+			},
+			success : function(encryptedPassword){
+				require('machinepack-gravatar').getImageUrl({
+					emailAddress:req.param('email')
+				}).exec({
+					error: function(err){
+						return res.negotiate(err);
+					},
+					success:function(gravatarUrl){
+						User.create({
+							name:req.param('name'),
+							email:req.param('email'),
+							password:encryptedPassword,
+							lastLoggedIn:new Date(),
+							gravatarUrl:gravatarUrl
+						},function userCreated(err,newUser){
+							if(err){
+								console.log(err);
+								return res.negotiate(err);
+							}
+							console.log("User added");
+							return res.json({
+								id : newUser.id
+							});
+						})
+					}
+				})
+			}
+		})
+	}
 };
 
--- User Authentication/api/models/User.js
+++ User Authentication/api/models/User.js
@@ -5,10 +5,31 @@
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
 
-module.exports = {
+ module.exports = {
 
-  attributes: {
-
-  }
-};
+ 	attributes: {
+ 		name:{
+ 			type:'string',
+ 			required:true
+ 		},
+ 		email:{
+ 			type:'string',
+ 			email:true,
+ 			required:true,
+ 			unique:true
+ 		},
+ 		password:{
+ 			type:'string',
+ 			required:true
+ 		},
+ 		lastLoggedIn:{
+ 			type:'date',
+ 			required:true,
+ 			defaultsTo:new Date(0)
+ 		},
+ 		gravatarUrl:{
+ 			type:'string'
+ 		}
+ 	}
+ };
 
