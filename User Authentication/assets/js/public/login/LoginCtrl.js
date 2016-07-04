125e960b169b9a79195d8047562a893e92229042
Ngo Hung Phuc <ngohungphuc95@gmail.com>
2016-07-04 15:50:33 +0700

    Frontend Login

---
+++ User Authentication/assets/js/public/login/LoginCtrl.js
@@ -0,0 +1,27 @@
+/**
+* SingupCtrl Module
+*
+* Description
+*/
+angular.module('LoginMod').controller('LoginCtrl', ['$scope','$http','toastr', function ($scope,$http,toastr) {
+	console.log('LoginCtrl init');
+	$scope.runLogin = function(){
+		$http.put('/login',{
+			email : $scope.email,
+			password : $scope.password
+		}).then(function onSuccess(){
+			window.location('/');
+		}).catch(function onError(err){
+			if(err.status == 400 || 404){
+				toastr.error("Invalid credentails","Error",{
+					closeButton : true
+				});
+				return;
+			}
+			toastr.error("Error has ocurred","Error",{
+					closeButton : true
+			});
+			return;
+		})
+	}
+}])
